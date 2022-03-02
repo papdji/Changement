53d9a9e09a285287f159d0e0ecab20c9e2baaf9c
Papdji <mbah0397@gmail.com>
2022-03-02 20:06:30 +0000

    localités

--- Mali-mobile/src/app/pages/home/home.module.ts
+++ Mali-mobile/src/app/pages/home/home.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-
+import { SwiperModule } from 'swiper/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
 
 @NgModule({
   imports: [
+    SwiperModule,
     CommonModule,
     FormsModule,
     IonicModule,
--- Mali-mobile/src/app/pages/home/home.page.ts
+++ Mali-mobile/src/app/pages/home/home.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController} from '@ionic/angular';
 import { LoadingController } from '@ionic/angular';
+import { getDatabase } from "firebase/database";
+import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper';
+
+SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom]);
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -9,11 +13,18 @@ import { LoadingController } from '@ionic/angular';
 export class HomePage implements OnInit {
   loadingCtrl: any;
   loading: any;
+  onSwiper([swiper]) {
+    console.log(swiper);
+  }
+  onSlideChange() {
+    console.log('slide change');
+  }
 
   constructor(public modalCtrl: ModalController, public loadingController: LoadingController) {
 
    }
 
   ngOnInit() {}
+
 
 }
